b5920b12d260680a4af683591b5ff1a2236f8d77
Zi <zyang25@stevens.edu>
2018-08-28 08:56:30 -0400

    Leetcode-35

---
+++ src/main/java/com/zyang25/L35_Search_Insert_Position.java
@@ -0,0 +1,28 @@
+package com.zyang25;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class L35_Search_Insert_Position {
+	
+	public static class Solution {
+
+		public int searchInsert(int[] nums, int target) {
+	    	SortedSet<Integer> r = new TreeSet<>();
+	    	
+	    	for(int n : nums){
+	    		r.add(n);
+	    	}
+	    	
+	    	if(r.contains(target))
+	    		return r.headSet(target).size();
+	    		
+	    	
+			return target;
+	    }
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
