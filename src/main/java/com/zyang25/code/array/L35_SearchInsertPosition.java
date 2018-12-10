97ddcb5a259f5fbe2c13f7316c5c4584516e2c39
Zi <zyang25@stevens.edu>
2018-12-09 21:39:40 -0500

    practice

--- src/main/java/com/zyang25/code/array/L35_SearchInsertPosition.java
+++ src/main/java/com/zyang25/code/array/L35_SearchInsertPosition.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zyang25.code.array;
 
 import java.util.Arrays;
@@ -55,59 +54,3 @@ public class L35_SearchInsertPosition {
 		
 	}
 }
-=======
-package com.zyang25.code.array;
-
-/*
- *  Example 1:
-
-	Input: [1,3,5,6], 5
-	Output: 2
-	Example 2:
-	
-	Input: [1,3,5,6], 2
-	Output: 1
-	Example 3:
-	
-	Input: [1,3,5,6], 7
-	Output: 4
-	Example 4:
-	
-	Input: [1,3,5,6], 0
-	Output: 0
-	
- * */
-public class L35_SearchInsertPosition {
-	
-	public static class Solution {
-
-		public static int searchInsert(int[] nums, int target) {
-	    	if(nums.length == 0)
-	    		return 0;
-	    	
-	    	int r = 0;
-	    	
-	    	for(int i = 0; i < nums.length; i++) {
-	    		if(target <= nums[i]) {
-	    			if(target == nums[i]) {
-	    				r = i;
-	    				break;
-	    			}
-	    			else if(target < nums[i]){
-	    				r = i - 1;
-	    				break;
-	    			}
-				}else {
-					r = i + 1;
-					break;
-				}
-	    	}
-	    	return r;
-	    }
-	}
-
-	public static void main(String[] args) {
-		
-	}
-}
->>>>>>> branch 'master' of https://github.com/zyang25/leetcode-java.git
