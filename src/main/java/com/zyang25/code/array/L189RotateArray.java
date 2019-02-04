e17e32dd50e383ce68f7aa7e04123a683c2e4df8
Nick <zyang25@icloud.com>
2019-02-03 23:20:48 -0500

    add solutions

---
+++ src/main/java/com/zyang25/code/array/L189RotateArray.java
@@ -0,0 +1,20 @@
+package com.zyang25.code.array;
+
+public class L189RotateArray {
+
+	public static void main(String[] args) {
+	}
+	
+	public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        
+        for(int i = 0; i < n; i++){
+            if(i + k - n > n) {
+                
+            } else {
+                
+            }
+        }
+    }
+
+}
