9be329510feb79da69ac26a205e0d7077159e768
Zi <zyang25@stevens.edu>
2019-02-18 20:12:23 -0500

    add comments to solution

--- src/main/java/com/zyang25/code/stack/L856ScoreOfParentheses.java
+++ src/main/java/com/zyang25/code/stack/L856ScoreOfParentheses.java
@@ -2,6 +2,12 @@ package com.zyang25.code.stack;
 
 import java.util.Stack;
 
+/**
+ * Use 2 stacks for calculation
+ * Stack 1 will valid the parenthese
+ * Stack 2 will keep track the index and value of that parenthese
+ * For Stack 1 and 2 they both use index of the array of tracking
+ */
 public class L856ScoreOfParentheses {
 	public static void main(String[] args) {
 		L856ScoreOfParentheses app = new L856ScoreOfParentheses();
@@ -10,22 +16,22 @@ public class L856ScoreOfParentheses {
 	
 	public int scoreOfParentheses(String S) {
 		Stack<Integer> s1 = new Stack<>();
-		Stack<Integer> s2 = new Stack<>();
+		Stack<int[]> s2 = new Stack<>();
 		char[] chs = S.toCharArray();
 		int r = 0;
 
 		for(int i = 0; i < S.length(); i++) {
 			if(s1.size() > 0 && chs[s1.peek()] == '(' && chs[i] == ')') {
 				if(i - s1.peek() == 1) {
-					s2.push(i);
+					s2.push(new int[]{i, 1});
 				} else {
 					int tmpR = 0;
-					while ( s2.size() > 0 && s2.peek() > s1.peek() ) {
-						tmpR += 1;
+					while ( s2.size() > 0 && s2.peek()[0] > s1.peek() ) {
+						tmpR += s2.peek()[1];
 						s2.pop();
 					}
 					tmpR *= 2;
-					r += tmpR;
+					s2.push(new int[]{i, tmpR});
 				}
 				s1.pop();
 			} else
@@ -33,7 +39,7 @@ public class L856ScoreOfParentheses {
 		}
 
 		while(s2.size() > 0) {
-			r += 1;
+			r += s2.peek()[1];
 			s2.pop();
 		}
 
