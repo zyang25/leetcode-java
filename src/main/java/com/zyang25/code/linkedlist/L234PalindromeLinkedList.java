9514fa1c813297c605031feefd82ab6f1960e381
Zi <zyang25@stevens.edu>
2019-05-27 22:25:36 -0400

    Reset base for Intellij

---
+++ src/main/java/com/zyang25/code/linkedlist/L234PalindromeLinkedList.java
@@ -0,0 +1,17 @@
+package com.zyang25.code.linkedlist;
+
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
