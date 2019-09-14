967cb47b405c0ed674574758d94b23bf50156207
Zi <zyang25@icloud.com>
2019-09-14 12:50:52 -0400

    add solutions

--- src/main/java/com/zyang25/code/linkedlist/L234PalindromeLinkedList.java
+++ src/main/java/com/zyang25/code/linkedlist/L234PalindromeLinkedList.java
@@ -1,17 +1,47 @@
 package com.zyang25.code.linkedlist;
 
+import com.zyang25.model.ListNode;
+
 /**
  * Definition for singly-linked list. public class ListNode { int val; ListNode
  * next; ListNode(int x) { val = x; } }
  */
-class Solution {
-    public boolean isPalindrome(ListNode head) {
-        return false;
+class L234PalindromeLinkedList {
+    public static void main(String[] args) {
+        L234PalindromeLinkedList app = new L234PalindromeLinkedList();
+
+        ListNode head = ListNode.create(1, 0, 1);
+
+        app.isPalindrome(head);
     }
 
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
+    public boolean isPalindromeFast(ListNode head) {
+        //reverse the 2nd part??
+
+        return true;
+    }
+    
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp = head;
+        ListNode newHead = new ListNode(head.val);
+
+        while (head.next != null) {
+            ListNode n = new ListNode(head.next.val);
+            n.next = newHead;
+            newHead = n;
+            head = head.next;
+        }
+
+        while (newHead != null) {
+            if(temp == null)
+                return false;
+            else if(temp.val != newHead.val)
+                return false;
+
+            newHead = newHead.next;
+            temp = temp.next;
+        }
+
+        return true;
     }
 }
